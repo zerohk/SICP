6465f71ec0867ad682774884d39b167c1edb0102
zerohk <forlovelygirlfyt@gmail.com>
2025-01-05 19:52:21 +0800

    更新笔记，添加if示例

---
+++ code/grade.rkt
@@ -0,0 +1,10 @@
+#lang sicp
+(define (grade x)
+            (cond ((>= x 90) 'A)
+                  ((>= x 80) 'B)
+                  ((>= x 70) 'C)
+                  ((>= x 60) 'D)
+                  (else 'F)))
+(grade 85)
+(grade 55)
+(grade 95)
